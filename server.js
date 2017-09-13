dee27310af28834624f33fc34576a40478bf1cfd
user <user@home.com>
2017-09-13 10:53:53 +0800

    Add project page

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use ((req, res, next) => {
   next();
 });
 
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
-})
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// })
 
 app.use(express.static(__dirname + '/public'));
 
@@ -53,6 +53,13 @@ app.get('/about', (req,res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
   });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page',
+    welcomeMessage: 'My Projects'
+  })
 });
 
 app.get('/bad', (req,res) => {
